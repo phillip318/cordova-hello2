34d9920589198ecc13dcbd45cfb3515d4a152d4a
luolu <101480024@qq.com>
2019-11-10 23:35:48 +0800

    first commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,38 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-hot-code-push-plugin.chcp",
+    "file": "plugins/cordova-hot-code-push-plugin/www/chcp.js",
+    "pluginId": "cordova-hot-code-push-plugin",
+    "clobbers": [
+      "chcp"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.4",
+  "cordova-plugin-device": "2.0.3",
+  "cordova-plugin-inappbrowser": "3.1.0",
+  "cordova-hot-code-push-plugin": "1.5.3"
+};
+// BOTTOM OF METADATA
+});
---
+++ www/js/index.js
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('chcp_updateIsReadyToInstall', this.onUpdateReady, false);
+        document.addEventListener('chcp_updateInstalled', this.onUpdateInstalled, false);
+        document.addEventListener('chcp_updateInstallFailed', this.onUpdateFailed, false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        console.log(app.checkForUpdate)
+        document.getElementById('myFetchBtn').addEventListener('click', app.checkForUpdate);
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    onUpdateReady: function() {
+        console.log('Update is ready for installation');
+    },
+    onUpdateInstalled :function () {
+        console.log('stalled')
+    },
+    onUpdateFailed: function () {
+        console.log('faild')
+    },
+    checkForUpdate: function() {
+        console.log(chcp)
+        chcp.fetchUpdate(this.fetchUpdateCallback);
+    },
+    fetchUpdateCallback: function(error, data) {
+        console.log(error, data)
+        if (error) {
+          console.log('Failed to load the update with error code: ' + error.code);
+          console.log(error.description);
+        } else {
+          console.log('Update is loaded');
+        }
+    }
+};
+
+app.initialize();
